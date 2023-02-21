0b498c845c9d0df0c901b08c6315c30cc883eeaa
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-21 12:02:42 -0800

    1 alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,11 @@
  *
  * Return: 0 (success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
 {
 	char c;
-	for (c = 'a'; c <= 'z'; c++){
+
+	for (c = 'a'; c <= 'z'; c++) {
 		_putchar(c);
 	}
 	_putchar('\n');
