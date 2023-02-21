a83eb28ea11cac8d99c684f8f86bc71b5618d459
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-21 05:07:39 -0800

    0 putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-	int (_putchar);
-	_putchar("_putchar");
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
 	return (0);
 }
