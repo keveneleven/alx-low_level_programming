514074fd6b99f0942637b9388dd4036316a254fb
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-21 10:32:26 -0800

    0 putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,10 +4,15 @@
  *
  * Return: 0 (Success)
  */
-int _putchar(void);
 int main(void)
 {
-	int _putchar();
-	_putchar("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');	
 	return (0);
 }
