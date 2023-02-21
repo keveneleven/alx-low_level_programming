bb1c699b33bfd518b2ea334a49f4272cc8cf64d3
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-21 14:16:56 -0800

    2 print alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
+	int c;
+	char 1;
 
-	for (c = 'a'; c <= 10; c++)
+	for (c = 0; c <= 9; c++)
 	{
-		_putchar(c);
+		for (1 = 'a'; 1 <= 'z'; c++)
+			_putchar(1);
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
