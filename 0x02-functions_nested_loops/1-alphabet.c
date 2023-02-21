9d010a8eb66c07d72262e67649598ce0bb680d53
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-21 11:26:50 -0800

    1 alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print the alphabet in lowercase, the a new line
+ *
+ * Return: 0 (success)
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a', c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
