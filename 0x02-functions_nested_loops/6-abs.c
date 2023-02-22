14f7e088735d6e70cbff814751121ac00f87166c
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-22 05:11:03 -0800

    6 abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,7 +5,14 @@
  *
  * Return: absolute value of ab
  */
-int _abs(int ab)
+int _abs(int c)
 {
-	return (ab * ((ab > 0) - (ab < 0)));
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+		return (c);
 }
