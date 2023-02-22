764ce7c45b43c465da5760882179bdb42cb0f342
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-22 07:00:02 -0800

    11 print to 98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n <= 98; n++)
+	for ( n <= 98; n++)
 	{
 	if (n == 98)
 	{
@@ -27,7 +27,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	for (; n >= 98; n--);
+	for (; n >= 98; n--)
 	{
 	if (n == 98)
 	{
