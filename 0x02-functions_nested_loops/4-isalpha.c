5577a2ac2067e9b88e0be4378c5debd3a1c8364d
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-22 04:23:43 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,17 +2,17 @@
 /**
  * _isalpha - function that checks for alphabetic character
  *
- * Return: 1 for alphabetic lowercase or uppercase character
+ * Return: 1 for alphabetic lowercase or uppercase character, 0 otherwise
  * @c: character to be checked
  */
 int _isalpha(int c)
 {
-	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c < 90))
 	{
 		return (1);
 	}
 	else
 	{
-		return (0);
+		return (0);
 	}
 }
