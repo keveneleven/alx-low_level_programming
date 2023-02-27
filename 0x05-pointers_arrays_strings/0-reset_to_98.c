780fde1dda888d4bc37ba79fbcf3242e45a133c5
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-27 09:56:17 -0800

    0 reset to 98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,8 +7,9 @@
  */
 void reset_to_98(int *n)
 {
-	int *n;
-	n = 98;
-	reset (&n);
-	_putchar("Reset value: %d\n", *n);
+	n = 402;
+	_putchar("n=%d\n", n);
+	reset_to_98(&n);
+	_putchar("n=%d\n", n);
+	return (0);
 }
