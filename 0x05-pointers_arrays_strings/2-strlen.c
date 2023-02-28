517c4cfaf425654430a0c9bf9fe8f372f2f999a2
Mnqobi Donga <kevedonga1017@gmail.com>
2023-02-27 16:10:28 -0800

    2 strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+/**
+ * int_strlen - return length of a string
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	char *s[100];
+	int length;
+
+	_putchar("Enter string to calculate its length\n");
+	length = strlen(*s);
+
+	_putchar("length of string = %d\n", length);
+	return (0);
+}
