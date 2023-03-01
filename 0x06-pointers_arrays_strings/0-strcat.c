b44a2119a6b6fbbcc770039451d1d4614f407933
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-01 12:53:04 -0800

    0 strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _ strcat - concatenates two strings
+ *
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *dest;
+	char *src;
+	char *strcat;
+
+	_strcat(dest, src);
+	_putchar("_strcat(dest, src): %s\n", dest);
+}
