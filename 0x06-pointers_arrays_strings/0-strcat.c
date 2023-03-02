4ad22572a495ba605bb9ae4a37668d02a1780395
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-01 18:00:30 -0800

    0 strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,12 +5,24 @@
  * @dest: pointer to character that will be changed
  * @src: pointer to a character that will be changed
  *
- * Return: 0
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
+	int m, n;
 
-	_strcat(dest, src);
-	return (0);
-
+	m = 0;
+	while (dest[m] != '\0')
+		{
+			m++
+		}
+	n = 0;
+	while (src[n] != '\0')
+	{
+		dest[m] = src[n];
+		m++;
+		n++;
+	}
+	dest[m] = '\0';
+	return (dest);
 }
