c6b1b2dfe91a073cf7cabc46222269a7a5c1fcd6
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-01 17:14:46 -0800

    0 strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,15 @@
 #include "main.h"
 #include <string.h>
 /**
- * _ strcat - concatenates two strings
+ * _strcat - concatenates two strings
+ * @dest: pointer to character that will be changed
+ * @src: pointer to a character that will be changed
  *
  * Return: 0
  */
 char *_strcat(char *dest, char *src)
 {
+
 	_strcat(dest, src);
 	return (0);
 
