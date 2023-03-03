f567e1bcfadd35875cd9bf4d8d8598d98607b842
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-02 18:39:03 -0800

    6 ca-_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *cap_string(char *s)
+{
+	int m = 0, n;
+	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	if (*(s + m) >= 97 && *(s + m) <= 122)
+		*(s + m) = *(s + m) - 32;
+	mt++;
+	while (*(s + m) != '\0')
+	{
+		for (n = 0; i < 13; i++)
+		{
+			if (*(s + m) == sep_words[n])
+			{
+				if ((*(s + (m + 1)) >= 97) && (*(s + (m + 1)) <= 122))
+					*(s + (m + 1)) = *(s + (m + 1)) - 32;
+				break;
+			}
+		}
+		m++;
+	}
+	return (s);
+}
+
