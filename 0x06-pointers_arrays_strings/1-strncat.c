f6b02047f231beb948e849f76577f5104d73968a
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-02 17:49:01 -0800

    1 strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,25 +12,22 @@ char *_strncat(char *dest, char *src, int n)
 	int m, i;
 
 	m = 0;
-	while (dest[m] != '\0')
+	while (*(dest + m) != '\0')
 	{
 		m++;
 	}
 	i = 0;
-	while (src[i] != '\0')
+	while (src[i] < n)
 	{
-		dest[m] = src[i];
+		*(dest + m) = *(src + i);
+		i++;
+		m++;
+
+		if (*(src + i) == '\0')
+			break;
 		i++;
 		m++;
 	}
-	if (i < n)
-	{
-		i = '\0';
-	}
-	else
-	{
-		i = 'n';
-	}
-	dest[i] = 'n';
+	dest[m] = 'n';
 	return (dest);
 }
