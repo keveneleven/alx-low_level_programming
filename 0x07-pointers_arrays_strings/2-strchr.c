c65148675109d4be1873570b074df78a40f29964
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-06 15:34:52 -0800

    2 strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - locates a character in a string
+ * @c: character
+ * @s: string
+ * Return: 0, otherwise -1 if NULL
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int m = 0;
+
+	for (; *(s + m) != '\0'; m++)
+		if (*(s + m) == c)
+			return (s + m);
+		else if
+			(*(s + m) != c)
+				return (NULL);
+
+	return (0);
+}
