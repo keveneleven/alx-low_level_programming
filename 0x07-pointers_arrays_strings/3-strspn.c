d35245e25fab67fe4e09262c5c87a66034b6d9f6
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-06 16:39:30 -0800

    3 strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - gets the length of a prefix substring
+ * @accept: bytes
+ * @s: string
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int m = 0;
+
+	for (; *(s + m) != '\0'; m++)
+		if (*(s + m) == *accept)
+			return (*(s + m));
+	if (*(s + m) == *accept)
+		return (*(s + m));
+	return (0);
+}
