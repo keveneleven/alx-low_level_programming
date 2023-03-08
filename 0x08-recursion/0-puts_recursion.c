c38e0046470f90995503faeb29f364b5acccb1fd
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-08 02:18:43 -0800

    0 puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - functiion that prints a string
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	_putchar("Puts with recursion"\n);
+	return (0);
+}
