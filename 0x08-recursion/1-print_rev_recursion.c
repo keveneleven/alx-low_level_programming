6304997222b8d75b5a11ebf2a179e26cbc36f6fe
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-08 04:19:47 -0800

    1 print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string
+ * Return: 0
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		putchar(*s);
+		_print_rev_recursion(s - 1);
+	}
+}
