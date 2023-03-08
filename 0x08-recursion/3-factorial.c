758eef8580c578b6386107de560fa3a1db22748b
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-08 14:41:02 -0800

    3 factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -11,14 +11,13 @@ int factorial(int n)
 	if (n == 0)
 	{
 		return (1);
-	
-		else
-	
-			!n = n * !(n - 1);
 	}
 	else if (n < 0)
 	{
-			return (-1);
+		return (-1);
 	}
-	return(0);
+	else
+	{
+		return (factorial = n * factorial(n - 1));
+	}
 }
