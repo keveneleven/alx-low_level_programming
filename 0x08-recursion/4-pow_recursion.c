4146f13085136386305a1cc122b1c52ed1163296
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-08 16:43:43 -0800

    4 pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -15,9 +15,8 @@ int _pow_recursion(int x, int y)
 	if (y == 0)
 	{
 		return (1);
-		_pow_recursion((x, y + 1) / x);
 	}
-	return (_pow_recursion(x, y - 1) * x);
+	return (x * _pow_recursion(x, y - 1));
 }
 
 
