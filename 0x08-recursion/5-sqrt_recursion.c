e6ab462148942d38bb12cbf7f1e5b15e5d036ec0
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-08 17:39:54 -0800

    5 sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,33 @@
 #include "main.h"
 #include <string.h>
+
+int actual_sqrt_recursion(int n, int m);
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: int
  * @m: iterate number
  * Return: If n does not have a natural square root, return -1
  */
-int _sqrt_recursion(int n, int m)
+int _sqrt_recursion(int n)
 {
-	int sqrt_recursion = m * m;
-
-	if (sqrt_recursion > n)
-	{
+	if (n < 0)
 		return (-1);
-	}
-	if (sqrt_recursion == n)
-	{
-		return (m);
-	}
-	return (_sqrt_recursion(n, m + 1));
+	return (actual_sqrt_recursion(n, 0));
 }
+/**
+ * actual_sqrt_recursion - to find natural 
+ * square root number
+ * @n: number to calculate root of
+ * @m: iterator
+ * Return: resulting square
+ */
+
+int actual_sqrt_recursion(int n, int m)
+{
+	if (m * m > n)
+		return (-1);
+	if (m * m == n)
+		return(m);
+	return (actual_sqrt_recursion(n, m + 1));
+}
+
