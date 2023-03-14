16885dcabdc64046f5fe4612ad9c39cea7033c15
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-14 11:24:26 -0700

    0 create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	s = malloc(sizeof(s) * size);
+	s = malloc(sizeof(c) * size);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
