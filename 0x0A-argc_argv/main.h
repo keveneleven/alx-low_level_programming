2bd44618e43c2a00bab0ecbf51ae89b1c07f94f3
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-15 09:59:59 -0700

    0 whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
