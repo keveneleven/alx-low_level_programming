d37b9fe5700cee76287b15f96d4bf911d115d885
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-16 07:22:04 -0700

    0 malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: int
+ *
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	unsigned int m;
+	int *i;
+
+	m =malloc(sizeof(i) * b)
+	if (malloc = NULL)
+		return (98);
+	return(0);
+}
