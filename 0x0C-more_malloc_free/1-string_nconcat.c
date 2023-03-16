95cdac34599931746a79da98a4faa63c1bb5bb33
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-16 12:11:39 -0700

    1 string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,10 +23,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (b = 0; s2[b] != '\0'; b++)
 		;
 	lens2 = b;
-        if (n >= lens2)
-                b = lens2;
-        else
-                b = n;
+	if (n >= lens2)
+		b = lens2;
+	else
+		b = n;
 	p = malloc(sizeof(char) * (a + b + 1));
 	if (p == NULL)
 	{
