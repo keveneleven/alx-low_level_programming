9aff602746968a6252ecf338d4cd97fce72d380a
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-16 17:41:28 -0700

    100-realloc.c root@19b3d8f559ac:/alx-low_level_programming/0x0C-more_malloc_free# root@19b3d8f559ac:/alx-low_level_programming/0x0C-more_malloc_free# git commit -m 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _realloc - reallocates a memory block
- * @ptr: pointer to memory 
- * @old_size: size in bytes
- * @new_size: new size in bytes
- * Return: pointer to newly allocated memory
+ * _realloc - reallocates a memory block.
+ * @ptr: pointer to the memory previously allocated.
+ * @old_size: size, in bytes, of the allocated space of ptr.
+ * @new_size: new size, in bytes, of the new memory block.
+ *
+ * Return: ptr.
+ * if new_size == old_size, returns ptr without changes.
+ * if malloc fails, returns NULL.
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	char *1ptr;
-	char *pre_ptr;
-	unsigned int m;
-
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (!ptr)
-		return (malloc(new_size));
-	1ptr = malloc(new_size);
-	if (!1ptr)
-		return (NULL);
-	pre_ptr = ptr;
-	if (new_size < old_size)
-	{
-		for (m = 0; m < new_size; m++)
-			1ptr[m] = pre_ptr[m];
-	}
-	if (new_size > old_size)
-	{
-		for (m = 0; m < old_size; m++)
-			1ptr[m] = pre_ptr[m];
-	}
+
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+
+	if (new_size == old_size)
+		return (ptr);
+
 	free(ptr);
-	return (1ptr);
+	ptr = malloc(new_size);
+
+	return (ptr);
 }
