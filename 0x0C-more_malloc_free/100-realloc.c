acc10da22b3b3d3e67fe2311b083a25c6df37dfa
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-16 17:25:58 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -23,6 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (!ptr)
 		return (malloc(new_size));
 	1ptr = malloc(new_size);
+	if (!1ptr)
 		return (NULL);
 	pre_ptr = ptr;
 	if (new_size < old_size)
