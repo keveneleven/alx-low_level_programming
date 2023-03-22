e127db1a9b8e406870a3ac956aab8659bdeebd6e
Mnqobi Donga <kevedonga1017@gmail.com>
2023-03-22 15:07:36 -0700

    100 main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	char *opc = (char *) main;
+	int m, bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	bytes = atoi(argv[1]);
+
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (m = 0; m < bytes; m++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (m != bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
