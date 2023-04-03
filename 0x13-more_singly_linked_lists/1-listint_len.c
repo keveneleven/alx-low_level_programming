a9f05ef54f42b02605b75744efefa97d731860c0
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-03 10:34:43 -0700

    task 1 done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib>
+/**
+ * listint_len - returns number of elements in a linked list
+ * @h: singly linked list
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t m;
+
+	m = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		m++;
+	}
+	return (m);
+}
