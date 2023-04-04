a5e52b4651d41804301791d9c4ec1a384a70ec7f
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-04 06:39:29 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int m;
 
 	node = head;
-	for (m = 0; m < index && node != NULL; m++;)
+	for (m = 0; m < index && node != NULL; m++)
 	{
 		node = node->next;
 	}
