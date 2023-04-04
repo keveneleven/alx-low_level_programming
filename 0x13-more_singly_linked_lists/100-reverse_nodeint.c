5caf6b1eb0ae898f89a09de04b9e8b7d4d2c58e1
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-04 09:34:49 -0700

    real 100-reverse_nodeint.c

---
+++ 0x13-more_singly_linked_lists/100-reverse_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a list
+ * @head: singly linked list
+ * Return: pointer to first node
+ * of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next = NULL;
+	listint_t *current = *head;
+
+	while (current != NULL)
+	{
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
+	}
+	*head = prev;
+	return (*head);
+}
