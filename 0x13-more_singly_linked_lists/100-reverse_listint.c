04e0c9488842f392e144f5477d3796934a89e7ce
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-04 09:38:48 -0700

    updated 100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -12,10 +12,10 @@ listint_t *reverse_listint(listint_t **head)
 
 	while (current != NULL)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 	*head = prev;
 	return (*head);
