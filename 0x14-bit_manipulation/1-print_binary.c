64fcf2804cd0839bc0213bec2117a76f862bb9f8
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-08 09:14:15 -0700

    added something

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -19,11 +19,11 @@ void print_binary(unsigned long int n)
 	{
 		if (count & n)
 		{
-			putchar('1');
+			_putchar('1');
 		}
 		else
 		{
-			putchar('0');
+			_putchar('0');
 		}
 		count >>= 1;
 	}
