c20bc10564d5f3671b886e90c55188534684f9fb
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-09 12:46:45 -0700

    4-clear_bit. fixed

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,7 +5,7 @@
  * to 0 at a given index
  * @n: printed num
  * @index: index of bit
- * return: 1 if it worked,
+ * Return: 1 if it worked,
  * or -1 if an error occurred
  */
 int clear_bit(unsigned long int *n, unsigned int index)
