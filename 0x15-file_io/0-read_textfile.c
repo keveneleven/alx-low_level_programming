8824b49d2c51c2dbf0c3b9dbe900612223818bda
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-10 05:12:29 -0700

    0-read_textfile.c fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,31 +7,43 @@
  * to the POSIX standard output.
  * @filename: filename
  * @letters: number of letters
- * Return: actual number of letters
+ * Return: actual number
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes, text;
 	int m;
+	char *buffer;
+	ssize_t bytes, text;
 
 	if (filename == NULL)
 		return (0);
 	m = open(filename, O_RDONLY);
 	if (m == -1)
+	{
 		return (0);
-	char buffer[letters];
+	}
+	buffer = (char *)malloc(letters * sizeof(char));
+	if (buffer == NULL)
+	{
+		close(m);
+		return (0);
+	}
 	bytes = read(m, buffer, letters);
 	if (bytes == -1)
 	{
 		close(m);
+		free(buffer);
 		return (0);
 	}
 	text = write(STDOUT_FILENO, buffer, bytes);
-	if (text == -1 || text != bytes)
+	if (bytes == -1 || text != bytes)
 	{
 		close(m);
+		free(buffer);
 		return (0);
 	}
 	close(m);
-	return (bytes);
+	free(buffer);
+	return (text);
 }
+
