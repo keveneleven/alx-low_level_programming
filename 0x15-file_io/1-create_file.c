066e649b08e6205dc131009c7745538e22304b1f
Mnqobi Donga <kevedonga1017@gmail.com>
2023-04-10 05:56:39 -0700

    1-create_file.c fixed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,8 +1,10 @@
 #include "main.h"
 #include <unistd.h>
+#include <fcntl.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <string.h>
 #include <sys/types.h>
 /**
  * create_file - creates a file
@@ -17,9 +19,10 @@ int create_file(const char *filename, char *text_content)
 {
 	ssize_t text;
 	int m;
-	if (filename == -1)
+
+	if (filename == NULL)
 		return (-1);
-	m = open(filename, O_CREATE | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	m = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (m == -1)
 	{
 		return (-1);
