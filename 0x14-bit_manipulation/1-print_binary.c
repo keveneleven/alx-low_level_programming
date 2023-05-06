7dd541b13c02affb6eb4ae525bc7d7bbfc7c0fb9
Mnqobi Donga <kevedonga1017@gmail.com>
2023-05-05 17:46:06 -0700

    1-print_binary.c fixed

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	unsigned long int index = 1ul << (sizeof(unsigned long int) * 8 - 1);
 	unsigned long int m;
 
-	for (m = 0; m < sizeof(unsigned long int) * 8; m++)
+	for (m = 0ul; m < sizeof(unsigned long int) * 8ul; m++)
 	{
 		putchar((n & index) ? '1' : '0');
 		index >>= 1;
